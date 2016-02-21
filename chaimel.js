5fac2523d7cb49fad867de88c0cc670eed902133
Peter Lyons <pete@peterlyons.com>
2016-02-21 09:30:21 -0700

    standard format. update deps.

--- chaimel.js
+++ chaimel.js
@@ -1,107 +1,107 @@
-var chai = require("chai");
-var Assertion = chai.Assertion.prototype;
+var chai = require('chai')
+var Assertion = chai.Assertion.prototype
 
 var properties = [
-  "arguments",
-  "empty",
-  "false",
-  "null",
-  "ok",
-  "true",
-  "undefined"
-];
+  'arguments',
+  'empty',
+  'false',
+  'null',
+  'ok',
+  'true',
+  'undefined'
+]
 
 var beThat = [
-  "above",
-  "an.instanceof",
-  "an.instanceOf",
-  "at.least",
-  "at.most",
-  "below",
-  "closeTo",
-  "greaterThan",
-  "instanceOf",
-  "instanceof",
-  "lessThan",
-  "within"
-];
+  'above',
+  'an.instanceof',
+  'an.instanceOf',
+  'at.least',
+  'at.most',
+  'below',
+  'closeTo',
+  'greaterThan',
+  'instanceOf',
+  'instanceof',
+  'lessThan',
+  'within'
+]
 
 var paths = [
-  "that.is.a",
-  "that.is.an",
-  "to.be.a",
-  "to.be.an",
-  "to.contain",
-  "to.deep.equal",
-  "to.equal",
-  "to.equal",
-  "to.have.deep.members",
-  "to.have.deep.property",
-  "to.have.keys",
-  "to.have.length",
-  "to.have.length.above",
-  "to.have.length.greater.than",
-  "to.have.length.of.at.least",
-  "to.have.length.of.at.most",
-  "to.have.length.within",
-  "to.have.members",
-  "to.have.own.property",
-  "to.have.property",
-  "to.include",
-  "to.match",
-  "to.respondTo",
-  "to.satisfy",
-  "to.throw"
-];
+  'that.is.a',
+  'that.is.an',
+  'to.be.a',
+  'to.be.an',
+  'to.contain',
+  'to.deep.equal',
+  'to.equal',
+  'to.equal',
+  'to.have.deep.members',
+  'to.have.deep.property',
+  'to.have.keys',
+  'to.have.length',
+  'to.have.length.above',
+  'to.have.length.greater.than',
+  'to.have.length.of.at.least',
+  'to.have.length.of.at.most',
+  'to.have.length.within',
+  'to.have.members',
+  'to.have.own.property',
+  'to.have.property',
+  'to.include',
+  'to.match',
+  'to.respondTo',
+  'to.satisfy',
+  'to.throw'
+]
 
-beThat.forEach(function(chain) {
-  paths.push("to.be." + chain);
-  paths.push("that.is." + chain);
-  paths.push("that.is.not." + chain);
-});
+beThat.forEach(function (chain) {
+  paths.push('to.be.' + chain)
+  paths.push('that.is.' + chain)
+  paths.push('that.is.not.' + chain)
+})
 
-function upFirst(word) {
-  return word[0].toUpperCase() + word.slice(1);
+function upFirst (word) {
+  return word[0].toUpperCase() + word.slice(1)
 }
 
-function chainToCamel(chain) {
-  var words = chain.split(".");
-  return words[0] + words.slice(1).map(upFirst).join("");
+function chainToCamel (chain) {
+  var words = chain.split('.')
+  return words[0] + words.slice(1).map(upFirst).join('')
 }
 
-function buildMethod(utils, chain) {
-  return function _method() {
-    var assert = new chai.Assertion(this._obj);
-    return utils.getPathValue(chain, assert).apply(assert, arguments);
-  };
+function buildMethod (utils, chain) {
+  return function _method () {
+    var assert = new chai.Assertion(this._obj)
+    return utils.getPathValue(chain, assert).apply(assert, arguments)
+  }
 }
 
-function buildProperty(utils, chain) {
-  return function _property() {
-    return utils.getPathValue(chain, new chai.Assertion(this._obj));
-  };
+function buildProperty (utils, chain) {
+  return function _property () {
+    return utils.getPathValue(chain, new chai.Assertion(this._obj))
+  }
 }
 
-function chaimel(_chai, utils) {
-  var method = buildMethod.bind(null, utils);
-  paths.forEach(function(path) {
-    utils.addMethod(Assertion, chainToCamel(path), method(path));
-    path = "not." + path;
-    utils.addMethod(Assertion, chainToCamel(path), method(path));
-  });
-  var property = buildProperty.bind(null, utils);
+function chaimel (_chai, utils) {
+  var method = buildMethod.bind(null, utils)
+  paths.forEach(function (path) {
+    utils.addMethod(Assertion, chainToCamel(path), method(path))
+    path = 'not.' + path
+    utils.addMethod(Assertion, chainToCamel(path), method(path))
+  })
+  var property = buildProperty.bind(null, utils)
   properties.forEach(function (name) {
-    var path = "to.be." + name;
-    utils.addMethod(Assertion, chainToCamel(path), property(path));
-    path = "not." + path;
-    utils.addMethod(Assertion, chainToCamel(path), property(path));
-  });
-  var existPath = "to.exist";
-  utils.addMethod(Assertion, chainToCamel(existPath), property(existPath));
-  existPath = "not." + existPath;
-  utils.addMethod(Assertion, chainToCamel(existPath), property(existPath));
+    var path = 'to.be.' + name
+    utils.addMethod(Assertion, chainToCamel(path), property(path))
+    path = 'not.' + path
+    utils.addMethod(Assertion, chainToCamel(path), property(path))
+  })
+  var existPath = 'to.exist'
+  utils.addMethod(Assertion, chainToCamel(existPath), property(existPath))
+  existPath = 'not.' + existPath
+  utils.addMethod(Assertion, chainToCamel(existPath), property(existPath))
 }
 
-chai.use(chaimel);
+chai.use(chaimel)
 
-module.exports = chai.expect;
+module.exports = chai.expect
--- chaimel.test.js
+++ chaimel.test.js
@@ -1,62 +1,62 @@
-var expect = require("./");
+var expect = require('./')
 
-function willThrow() {
-  throw new Error();
+function willThrow () {
+  throw new Error()
 }
 
-describe("the chaimel assertion extensions", function () {
+describe('the chaimel assertion extensions', function () {
   [
-    [42, "toEqual", 42],
-    [42, "notToEqual", 43],
-    [42, "toEqual", 42],
-    [42, "toBeAbove", 41],
-    [42, "toBeGreaterThan", 41],
-    [42, "notToBeAbove", 43],
-    ["abc", "toInclude", "b"],
-    ["abc", "toMatch", /b/],
-    [false, "toBeFalse"],
-    [true, "toBeTrue"],
-    [arguments, "toBeArguments"],
-    [[1, 2, 3], "toHaveLengthOfAtMost", 3],
-    [[1, 2, 3], "toHaveLengthWithin", 2, 4],
-    [[1, 2, 3], "toHaveLength", 3],
-    [{foo: {bar: [42]}}, "toHaveDeepProperty", "foo.bar[0]"],
-    [42, "toBeWithin", 41, 43],
-    [[], "toBeInstanceof", Array],
-    [[], "toBeInstanceOf", Array],
-    [[], "toBeAnInstanceOf", Array],
-    ["", "toBeA", "string"],
-    [{}, "toBeAn", "object"],
-    [[], "toBeAnInstanceof", Array],
-    [{foo: 1, bar: 2}, "toHaveKeys", "foo", "bar"],
-    [willThrow, "toThrow", Error],
-    [[{id: 1}], "toHaveDeepMembers", [{id: 1}]],
-    [{id: 1}, "toHaveProperty", "id", 1],
-    [null, "toBeNull"],
-    [42, "notToBeNull"],
-    [42, "toBeOk"],
-    [void 0, "toBeUndefined"],
-    [42, "toExist"],
-    [void 0, "notToExist"]
-  ].forEach(function(set) {
-    it("should support " + set[1], function() {
-      var assert = expect(set[0]);
-      assert[set[1]].apply(assert, set.slice(2));
-    });
-  });
-  it("should support chaining to thatIsA", function() {
-    expect({id: 1}).toHaveProperty("id").thatIsA("number");
-  });
-  it("should support chaining to thatIsAn", function() {
-    expect({list: []}).toHaveProperty("list").thatIsAn("array");
-  });
-  it("should support chaining to thatIsGreaterThan", function() {
-    expect({id: 1}).toHaveProperty("id").thatIsGreaterThan(0);
-  });
-  it("should support chaining to thatIsAnInstanceOf", function() {
-    expect({list: [1, 2]}).toHaveProperty("list").thatIsAnInstanceOf(Array);
-  });
-  it("should support chaining to thatIsNotBelow", function() {
-    expect({count: 9}).toHaveProperty("count").thatIsNotBelow(8);
-  });
-});
+    [42, 'toEqual', 42],
+    [42, 'notToEqual', 43],
+    [42, 'toEqual', 42],
+    [42, 'toBeAbove', 41],
+    [42, 'toBeGreaterThan', 41],
+    [42, 'notToBeAbove', 43],
+    ['abc', 'toInclude', 'b'],
+    ['abc', 'toMatch', /b/],
+    [false, 'toBeFalse'],
+    [true, 'toBeTrue'],
+    [arguments, 'toBeArguments'],
+    [[1, 2, 3], 'toHaveLengthOfAtMost', 3],
+    [[1, 2, 3], 'toHaveLengthWithin', 2, 4],
+    [[1, 2, 3], 'toHaveLength', 3],
+    [{foo: {bar: [42]}}, 'toHaveDeepProperty', 'foo.bar[0]'],
+    [42, 'toBeWithin', 41, 43],
+    [[], 'toBeInstanceof', Array],
+    [[], 'toBeInstanceOf', Array],
+    [[], 'toBeAnInstanceOf', Array],
+    ['', 'toBeA', 'string'],
+    [{}, 'toBeAn', 'object'],
+    [[], 'toBeAnInstanceof', Array],
+    [{foo: 1, bar: 2}, 'toHaveKeys', 'foo', 'bar'],
+    [willThrow, 'toThrow', Error],
+    [[{id: 1}], 'toHaveDeepMembers', [{id: 1}]],
+    [{id: 1}, 'toHaveProperty', 'id', 1],
+    [null, 'toBeNull'],
+    [42, 'notToBeNull'],
+    [42, 'toBeOk'],
+    [void 0, 'toBeUndefined'],
+    [42, 'toExist'],
+    [void 0, 'notToExist']
+  ].forEach(function (set) {
+    it('should support ' + set[1], function () {
+      var assert = expect(set[0])
+      assert[set[1]].apply(assert, set.slice(2))
+    })
+  })
+  it('should support chaining to thatIsA', function () {
+    expect({id: 1}).toHaveProperty('id').thatIsA('number')
+  })
+  it('should support chaining to thatIsAn', function () {
+    expect({list: []}).toHaveProperty('list').thatIsAn('array')
+  })
+  it('should support chaining to thatIsGreaterThan', function () {
+    expect({id: 1}).toHaveProperty('id').thatIsGreaterThan(0)
+  })
+  it('should support chaining to thatIsAnInstanceOf', function () {
+    expect({list: [1, 2]}).toHaveProperty('list').thatIsAnInstanceOf(Array)
+  })
+  it('should support chaining to thatIsNotBelow', function () {
+    expect({count: 9}).toHaveProperty('count').thatIsNotBelow(8)
+  })
+})
